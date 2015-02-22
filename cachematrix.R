4a1858e527dca984ec69374be1f1f5a8ef2f052c
amirsarajan <amirhossein.sarajan@gmail.com>
2015-02-22 18:32:06 +0330

    programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the makeCacheMatrix function creates a cacheMatrix wrapping object for it's input matrix x
+## cacheSolve is also a wrapping method that tries to return a matrix's inverse from its cache.
 
-## Write a short comment describing this function
+
+## creates a wrapping cacheMatrix object
+## get method returns the original x matrix
+## set/getreverse store and retrieve the cached inverse from the original matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	get <- function() x
+	set <- function(y) x <<- y
+	setreverse <- function(r) attr(x,"reverse") <<- r
+	getreverse <- function() attr(x,"reverse")
+	list(set= set,
+		 get = get,
+		 setreverse = setreverse,
+		 getreverse = getreverse )
 }
 
 
-## Write a short comment describing this function
+## A wrapping method that tries to resolves the cached inverse from the original matrix through getreverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ # ## Return a matrix that is the inverse of 'x'
+  if(!is.null( x$getreverse()))   
+	x$getreverse()
+  else  
+    x$setreverse(solve(x$get()))	   
 }
+
+
+   
